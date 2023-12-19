a7f6335c9586fffc0f87a4958524aa9287319cde
Artem Glazychev <artem.glazychev@xored.com>
2023-12-19 19:39:05 +0700

    spire test
    Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>

--- go.mod
+++ go.mod
@@ -2,10 +2,9 @@ module github.com/networkservicemesh/integration-k8s-aks
 
 go 1.20
 
-require (
-	github.com/networkservicemesh/integration-tests v0.0.0-20231206083754-7c08d32b8b4b
-	github.com/stretchr/testify v1.8.1
-)
+require github.com/networkservicemesh/integration-tests v0.0.0-20231219115352-a3a362bd381d
+
+replace github.com/networkservicemesh/integration-tests => github.com/glazychev-art/integration-tests v0.0.0-20231221062347-605e06f2e841
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -15,6 +14,7 @@ require (
 	github.com/networkservicemesh/gotestmd v0.0.0-20220628095933-eabbdc09e0dc // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main_test.go
+++ main_test.go
@@ -21,15 +21,9 @@ package main_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/suite"
-
 	"github.com/networkservicemesh/integration-tests/extensions/parallel"
-	"github.com/networkservicemesh/integration-tests/suites/afxdp"
 	"github.com/networkservicemesh/integration-tests/suites/basic"
 	"github.com/networkservicemesh/integration-tests/suites/features"
-	"github.com/networkservicemesh/integration-tests/suites/heal"
-	"github.com/networkservicemesh/integration-tests/suites/memory"
-	"github.com/networkservicemesh/integration-tests/suites/observability"
 )
 
 func TestRunFeatureSuite(t *testing.T) {
@@ -40,18 +34,18 @@ func TestRunBasicSuite(t *testing.T) {
 	parallel.Run(t, new(basic.Suite))
 }
 
-func TestRunAfxdpSuite(t *testing.T) {
-	parallel.Run(t, new(afxdp.Suite))
-}
-
-func TestRunMemorySuite(t *testing.T) {
-	parallel.Run(t, new(memory.Suite))
-}
-
-func TestRunHealSuite(t *testing.T) {
-	suite.Run(t, new(heal.Suite))
-}
-
-func TestRunObservabilitySuite(t *testing.T) {
-	suite.Run(t, new(observability.Suite))
-}
+//func TestRunAfxdpSuite(t *testing.T) {
+//	parallel.Run(t, new(afxdp.Suite))
+//}
+//
+//func TestRunMemorySuite(t *testing.T) {
+//	parallel.Run(t, new(memory.Suite))
+//}
+//
+//func TestRunHealSuite(t *testing.T) {
+//	suite.Run(t, new(heal.Suite))
+//}
+//
+//func TestRunObservabilitySuite(t *testing.T) {
+//	suite.Run(t, new(observability.Suite))
+//}
